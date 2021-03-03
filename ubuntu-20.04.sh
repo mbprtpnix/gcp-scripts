7775fa55223e5c11cc6cf174e170fed97cc871da
mbprtpnix <78172971+mbprtpnix@users.noreply.github.com>
2021-03-03 11:34:10 +0200

    ...

--- ubuntu-20.04.sh
+++ ubuntu-20.04.sh
@@ -2,31 +2,4 @@
 
 apt update && apt upgrade -y
 
-apt install htop vim neovim bash-completion -y
-
-apt-get update -y
-
-apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common
-    
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-
-apt-key fingerprint 0EBFCD88
-
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-
-apt-get update -y
-
-apt-get install docker-ce docker-ce-cli containerd.io
-
-curl -L "https://github.com/docker/compose/releases/download/1.28.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
-chmod +x /usr/local/bin/docker-compose
-
+apt install htop vim neovim bash-completion wireguard-tools -y
